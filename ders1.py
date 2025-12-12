4a40996b094ca1d04ed14303da935a82903e83b7
Mehmet1042 <m.aliornek10@gmail.com>
2025-12-12 23:01:26 +0300

    Add files via upload

---
+++ ders1.py
@@ -0,0 +1,23 @@
+isim="Mehmet Ali"
+soyad="Örnek"
+TC_KİMLİK_NO="12345674206"
+doğum_tarihi="20.04.2010"
+doğum_yeri="Konya,Selçuklu"
+Baba_Adı="Fatih"
+Anne_Adı="Hacer"
+Kardeş_sayısı="1"
+Kardeş_adı="Ayşegül"
+
+print("=============================Kimlik Kartı==============") 
+
+print("ADI            :",isim)
+print("SOYAD          :",soyad)
+print("TC KİMLİK NO   :",TC_KİMLİK_NO)
+print("DOĞUM TARİHİ   :",doğum_tarihi)
+print("DOĞUM YERİ     :",doğum_yeri)
+print("BABA ADI       :",Baba_Adı)
+print("ANNE ADI       :",Anne_Adı)
+print("KARDEŞ SAYISI  :",Kardeş_sayısı)
+print("KARDEŞ ADI     :",Kardeş_adı)
+
+print("===========================================")
